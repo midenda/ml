8e5ad208ac1d10bab7ade6474c17de0756f27edc
Zorlix <zorlix.k@gmail.com>
2021-08-26 00:46:26 +0100

    fix memory leak

--- ml.cpp
+++ ml.cpp
@@ -34,7 +34,8 @@ float weighted_sum (float values[], float weights [], size_t length)
 {
     float accumulated = 0;
 
-    for (int i = 0; i < length; i++) {
+    for (int i = 0; i < length; i++) 
+    {
         accumulated += values [i] * weights [i];
     };
 
@@ -56,16 +57,16 @@ struct Layer
         size [1] = N;
         activations = new float [M]();
 
+        weights = new float* [M];
+        weights [0] = new float [M * N];
+
+        for (int i = 1; i < M; i++)
+        {
+            weights [i] = weights [i - 1] + N; 
+        };
+
         if (p == nullptr) 
         {
-            weights = new float* [M];
-            weights [0] = new float [M * N];
-
-            for (int i = 1; i < M; i++)
-            {
-                weights [i] = weights [i - 1] + N; 
-            };
-
             for (int i = 0; i < M * N; i++)
             {
                 weights [0][i] = RandomWeight ();
@@ -74,13 +75,26 @@ struct Layer
 
         else 
         {
-            weights = p;
+            for (int i = 0; i < M; i++)
+                for (int j = 0; j < N; j++)
+                    weights [0][i * N + j] = p [i][j];
         };
     };
 
     Layer () {};
 
-    float RandomWeight () {
+    Layer (const Layer &l) {
+        std::cout << "Copying Layer! " << std::endl;
+    };
+
+    ~Layer ()
+    {
+        delete [] weights [0];
+        delete [] weights;
+    };
+
+    float RandomWeight () 
+    {
         return 0;
     };
 };
@@ -89,55 +103,64 @@ struct Layer
 template <size_t depth>
 struct Network 
 {
-    Layer layers [depth];
+    Layer* layers [depth];
 
     size_t* dim;
 
     Network (size_t dimensions [depth + 1], activation_fn functions [depth]) 
         : dim (dimensions)
     {
-        for (int i = 0; i < depth; i++) {
-
+        for (int i = 0; i < depth; i++) 
+        {
             size_t M = dim [i + 1];
             size_t N = dim [i];
 
             activation_fn f = functions [i];
 
-            layers [i] = Layer (nullptr, M, N, f);
+            layers [i] = new Layer (nullptr, M, N, f);
         };
     };
 
-    void calc_next_layer (float input [], Layer l, size_t size [2]) 
+    ~Network ()
+    {
+        for (int i = 0; i < depth; i++)
+        {
+            delete layers [i];
+        };
+    };
+
+    void calc_next_layer (float input [], Layer* l, size_t size [2]) 
     {
         size_t M = size [0];
         size_t N = size [1];
 
-        for (int i = 0; i < M; i++) {
-            l.activations [i] = l.fn (weighted_sum (input, l.weights [i], N));
+        for (int i = 0; i < M; i++) 
+        {
+            l -> activations [i] = l -> fn (weighted_sum (input, l -> weights [i], N));
         };
     };
 
     void propagate (float input []) 
     {
-        for (int i = 0; i < depth; i++) {
+        for (int i = 0; i < depth; i++) 
+        {
+            Layer* l = layers [i];
+            calc_next_layer (input, l, l -> size);
 
-            Layer l = layers [i];
-            calc_next_layer (input, l, l.size);
-
-            input = l.activations;
+            input = l -> activations;
         };
     };
 
 
     void PrintOutput () 
     {
-        Layer l = layers [depth - 1];
+        Layer* l = layers [depth - 1];
 
-        size_t M = l.size [0];
+        size_t M = l -> size [0];
 
         for (int i = 0; i < M; i++)
         {
-            std::cout << l.activations [i];
+            std::cout << l -> activations [i];
         };
 
         std::cout << std::endl;
